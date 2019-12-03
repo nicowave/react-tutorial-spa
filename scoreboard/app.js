e666fe28e691a5cd4bbc4a28d61cce7b05c8806c
Nico Roldos <nroldos@esri.com>
2019-12-02 17:43:07 -0800

    add props, supply props to components, add data structure

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,41 +1,89 @@
 // app.js
 
-// components are often written as => function expresssions
+const players = [
+  {
+    name: "Nico",
+    score: 200
+  },
+  {
+    name: "Jim",
+    score: 85
+  },
+  {
+    name: "Lucian",
+    score: 95
+  },
+  {
+    name: "James",
+    score: 80
+  }
+];
+
+// Components are often written as => function expresssions
 // i.e. (see below)
 // function Header() { }...
+const App = (props) => {
+    return (
+        <div className="scoreboard">
+            <Header 
+              title="Scoreboard" 
+              totalPlayers={ props.initialPlayers.length } 
+            />
+            {/* Players list */}
+            { props.initialPlayers.map( player => 
+                // implicit return...
+                <Player 
+                 name={player.name} 
+                 score={player.score}
+                />
+            )}
+        </div>
+    );
+}
 
-const Header = () => {
+
+const Header = (props) => {
     return (
       <header>
-         <h1>Scoreboard</h1>
-         <span className="stats">Players: 1</span>
-      
+         <h1>
+             { props.title }
+         </h1>
+        <span className="stats">
+            Players: { props.totalPlayers}
+        </span>
       </header>
     ); 
  }
- 
- 
-const Player = () => {
+
+
+const Player = (props) => {
     return (
         <div className="player">
             {/* Player component */}
             <span className="player-name">
-                { playerName }
+              { props.name }
             </span>
-            {/* Counter component */}
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score"> 35 </span>
-                <button className="counter-action increment"> + </button>
-            </div>
+            {/* composition; rendering child component
+             inside parent component*/}
+            <Counter score={ props.score } />
         </div>
+    );   
+}
 
-    );
+const Counter = (props) => {
+    return (
+      <div className="counter">
+        {/* Counter component */}
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score"> { props.score } </span>
+        <button className="counter-action increment"> + </button>
+     </div>
+  );
 }
 
  
  ReactDOM.render(
-   <Player />,
+   <App initialPlayers={players} />,
    document.getElementById('root')
  );
  
